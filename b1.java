2ea3380a6dfec6ff4760190ff0aa5105bc3b381b
XLM <khoado.devie@gmail.com>
2026-01-08 11:24:10 +0700

    Add files via upload

---
+++ b1.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double radius = 5;
+        double area = Math.PI * radius * radius;
+        System.out.println(area);
+    }
+}
